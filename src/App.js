e2d9df0c1c52c05100a1e11ddc4747c4ccb2cb7d
fimoefive <brothermj@hotmail.com>
2021-04-19 15:36:02 -0500

    added Counter name input field and setCounterName= useState in Counter.js file.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-
 import './App.css';
 import Counter from './Counter';
 
 function App() {
 
+  // const arrayC = [
+  //   {
+  //     appName: "Counter 1",
+  //     counterValue: 200
+  //   },
+  //   {
+  //     appName: "Counter 2",
+  //     counterValue: 100
+  //   },
+  //   {
+  //     appName: "Counter 3",
+  //     counterValue: 200
+  //   },
+  //   {
+  //     appName: "Counter 4",
+  //     counterValue: 250
+  //   }
+  // ];
+
+  // {
+  //   arrayC.map((counter, i) => <Counter key={i} appName={counter.appName} counterValue={counter.counterValue} />)
+  // }
+
   return (
     <>
       <Counter />
-      <Counter appName="counter 1" initialValue={200} />
+
+      {/* <Counter appName="counter 1" initialValue={200} />
       <Counter appName="counter 2" initialValue={200} />
-      <Counter appName="counter 3" initialValue={200} />
+      <Counter appName="counter 3" initialValue={200} /> */}
+
     </>
   );
 };
+
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <div>
+
+//       </div>
+//     )
+//   }
+// };
 
 export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,43 +1,67 @@
 import React from 'react';
 import { useState } from 'react';
+// import React, { Component } from 'react';
 import logo from './logo.svg';
 
-export default function Counter(appName, counterValue) {
-  const cohort = 14;
-  const [counter, setCounter] = useState(counterValue);
-  const [counterName, setCounterName] = useState(appName);
-  const [userInput, setUserInput] = useState(counterValue);
+// export default function Counter(appName, counterValue) {
+export default function Counter() {
+  const cohort = "E14";
+  const [counter, setCounter] = useState(0);
+  const [counterName, setCounterName] = useState("Counter");
+  const [userInput, setUserInput] = useState();
 
   return (
     <>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1>Welcome to React Cohort {cohort} !!</h1>
-          <h3>Counter</h3>
-          <input onChange={(e) => setCounterName(e.target.value)}></input>
-          {counterName}
+      {/* <div className="App"> */}
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        {cohort === "E14" ? <h1>Welcome to React Cohort {cohort} !!</h1> : <h1>Day Cohort!</h1>}
+        {/* <h2>COUNTER</h2> */}
+
+        <h3>{counterName}</h3>
+
+        <h3>{counter}</h3>
+
+        <h4>Input Number</h4>
+        <input onChange={(e) => setUserInput(Number(e.target.value))} placeholder="Enter Number"></input>
+
+        <br />
+        <h4>Counter Name:</h4>
+        <input onChange={(e) => setCounterName(e.target.value)} placeholder="Enter Name"></input>
+
+
+        {counter <= 0 ? '' :
+          <button onClick={() => setCounter((prevState) => prevState + userInput)}>Increment by
+          {userInput}
+          </button>
+        }
+
+        {/*
           {counter <= 0 ? '' :
-            <button onClick={() => setCounter((prevState) => prevState + userInput)}>Increment by {userInput}
+            <button onClick={() => setCounter((prevState) => prevState - userInput)}>Decrease by
+          {userInput}
             </button>
           }
+          */}
 
-          <input onChange={(e) => setUserInput(Number(e.target.value))}></input> <br />
-          <button onClick={() => setCounter((prevState) => prevState - 1)}>Decrease by 1</button>
-          <button onClick={() => setCounter(0)}>Reset</button>
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+        <br></br>
+        <button onClick={() => setCounter(0)}>Reset</button>
+        <br></br>
+
+        {/* <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p> */}
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
         </a>
-        </header>
-      </div>
+      </header>
+      {/* </div> */}
     </>
   );
-}
+};
